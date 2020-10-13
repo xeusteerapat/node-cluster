54066ae0a0a53710c8bb6bc441933815fe083315
xeusteerapat <xeus085@gmail.com>
2020-10-13 15:35:24 +0700

    [update] refactor long operations with crypto

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
+const { pbkdf2 } = require('crypto');
 
 // Is the file being executed in master mode?
 if (cluster.isMaster) {
   // Cause index.js to be executed "again" but
   // in child mode
   cluster.fork();
-  // cluster.fork();
+  // cluster.fork(); // uncomment to add more children
   // cluster.fork();
   // cluster.fork();
 } else {
@@ -15,15 +17,17 @@ if (cluster.isMaster) {
   const app = express();
 
   // block the event loop
-  function doWork(duration) {
-    const start = Date.now();
+  // function doWork(duration) {
+  //   const start = Date.now();
 
-    while (Date.now() - start < duration) {}
-  }
+  //   while (Date.now() - start < duration) {}
+  // }
 
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi, Node.js');
+    // doWork(5000);
+    pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi, Node.js');
+    });
   });
 
   app.get('/fast', (req, res) => {
