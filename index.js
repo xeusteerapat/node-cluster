3af11d7f87db274ead87f5ad84e0a33d4d82c47e
xeusteerapat <xeus085@gmail.com>
2020-10-13 15:20:53 +0700

    [update] cluster implementation

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-// block the event loop
-function doWork(duration) {
-  const start = Date.now();
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed "again" but
+  // in child mode
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+} else {
+  // I'm a child, I'm going to act like a server
+  // and do nothing else
+  const express = require('express');
+  const app = express();
 
-  while (Date.now() - start < duration) {}
+  // block the event loop
+  function doWork(duration) {
+    const start = Date.now();
+
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi, Node.js');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('In the fast route!');
+  });
+
+  app.listen(5000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi, Node.js');
-});
-
-app.listen(5000);
