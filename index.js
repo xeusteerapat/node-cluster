da7314a53aa86326ca9a33cfcf4d38a21195231b
xeusteerapat <xeus085@gmail.com>
2020-10-13 15:04:45 +0700

    [update] example function that block event loop

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+// block the event loop
+function doWork(duration) {
+  const start = Date.now();
+
+  while (Date.now() - start < duration) {}
+}
+
 app.get('/', (req, res) => {
+  doWork(5000);
   res.send('Hi, Node.js');
 });
 
